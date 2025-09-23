421143362554a821684784c446a46139116965b7
Andrew Baumbach <andrewb@twingate.com>
2025-09-23 15:27:42 -0400

    chore: define required providers

---
+++ digital_ocean/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    twingate = {
+      source  = "twingate/twingate"
+      version = "~> 3.0"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the Twingate Provider
+provider "twingate" {
+  api_token = var.tg_api_token
+  network   = var.tg_network
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
