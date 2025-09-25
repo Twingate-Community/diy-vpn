58051967d708516e47d242486a55252a605719a9
Andrew Baumbach <andrewb@twingate.com>
2025-09-25 11:47:03 -0400

    chore: update config to support auto scaling

--- digital_ocean/kubernetes/main.tf
+++ digital_ocean/kubernetes/main.tf
@@ -13,11 +13,14 @@ resource "digitalocean_kubernetes_cluster" "clusters" {
 
   name    = each.key
   region  = each.value.region
-  version = "1.33.1-do.3"
+  version = "1.33.1-do.4"
 
   node_pool {
     name       = "${each.key}-connector-pool"
     size       = each.value.node_size
     node_count = each.value.node_count
+    auto_scale = each.value.auto_scale
+    min_nodes  = each.value.min_count
+    max_nodes  = each.value.max_count
   }
 }
